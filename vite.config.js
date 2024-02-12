581bb3bd2326001603cc8b89e4ae0f93a2eddcab
94Jenn <151082727+94Jenn@users.noreply.github.com>
2024-02-12 10:12:29 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/módulo3-evaluación final/",
+  base: "/modulo-3-evaluacion-final-94Jennn/",
 })
